8491bde1c736377149076d66fdb2be0b42caf453
shrutisayare01 <shrutisayare4652@gmail.com>
2025-12-19 22:41:54 +0530

    handling side effect with useeffect

---
+++ Counter-with-Alert/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Counter from './Counter'
+function App() {
+
+  return (
+    <>
+       <div>
+      <Counter />
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ Userdata-API-Fetch/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import UserData from './UserData'
+
+function App() {
+
+  return (
+    <>
+       <div>
+      <UserData />
+    </div>
+    </>
+  )
+}
+
+export default App
